8268be9d0a295ca13defd5ad6526dbd85505f77d
moyumo <moyumon.i@gmail.com>
2018-05-24 14:26:08 +0900

    dotinstall#5

--- main.js
+++ main.js
@@ -3,11 +3,24 @@
 	var vm = new Vue({
 		el: '#app',
 		data:{
+			newItem: '',
 			todos:[
 				'task1',
 				'task2',
 				'task3'
 			]
+		},
+		methods: {
+			// # submitしても画面遷移させない
+			// addItem: function(e){
+			// 	e.preventDefault();
+			// 	this.todos.push(this.newItem);
+			// }
+			// ->ディレクティブにpreventつけちゃう
+			addItem: function(){
+				this.todos.push(this.newItem);
+				this.newItem = ''; //そのままだと値が残っちゃうからここで空文字にする
+			}
 		}
 	});
 })();
