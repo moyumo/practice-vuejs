84186e615970fb82a21fd6d70b9ca7e6cfd24bbc
moyumo <moyumon.i@gmail.com>
2018-05-24 14:36:37 +0900

    dotinstall#6

--- main.js
+++ main.js
@@ -11,6 +11,7 @@
 			]
 		},
 		methods: {
+			// == TODOを追加する
 			// # submitしても画面遷移させない
 			// addItem: function(e){
 			// 	e.preventDefault();
@@ -20,7 +21,14 @@
 			addItem: function(){
 				this.todos.push(this.newItem);
 				this.newItem = ''; //そのままだと値が残っちゃうからここで空文字にする
+			},
+			// // == TODO削除
+			deleteItem: function(index){
+				if(confirm('OK?')){
+					this.todos.splice(index,1); //index番目から1個削除
+				}
 			}
+
 		}
 	});
 })();
