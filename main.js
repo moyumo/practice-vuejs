d36843ac80e004164d079fee86a5b7104732c8a9
moyumo <moyumon.i@gmail.com>
2018-05-30 13:15:26 +0900

    dotinstall#15

--- main.js
+++ main.js
@@ -3,11 +3,17 @@
 	// Components
 
 	var likeComponent = Vue.extend({
-		template: '<button>like</button>'
-		//templateにかける要素は１つのみ
-		//複数かきたいときは
-		//<div><button>1</button><button>2</button></div>
-		//のように親要素で囲う
+		data: function(){
+			return{
+				count: 0
+			}
+		},
+		template: '<button @click=countUp>like {{ count }}</button>',
+		methods: {
+			countUp: function(){
+				this.count++;
+			}
+		}
 	});
 	var app = new Vue({
 		el: '#app',
