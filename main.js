0528ba4448922557eb0f6aafb5aba84188f647bb
moyumo <moyumon.i@gmail.com>
2018-05-30 13:20:56 +0900

    dotinstall#16

--- main.js
+++ main.js
@@ -3,12 +3,20 @@
 	// Components
 
 	var likeComponent = Vue.extend({
+		//props: ['message'],//カスタム属性
+		props: {
+			//オブジェクト形式で、型や値を指定できる
+			message: {
+				type: String,
+				default: 'Like'
+			}
+		},
 		data: function(){
 			return{
 				count: 0
 			}
 		},
-		template: '<button @click=countUp>like {{ count }}</button>',
+		template: '<button @click=countUp>{{message}} {{ count }}</button>',
 		methods: {
 			countUp: function(){
 				this.count++;
