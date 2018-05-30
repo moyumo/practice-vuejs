79565347b034dddcb7da4b4965d833f16daafb23
moyumo <moyumon.i@gmail.com>
2018-05-30 13:12:22 +0900

    dot install #14

--- main.js
+++ main.js
@@ -1,58 +1,19 @@
 (function(){
 	'use strict';
-	var vm = new Vue({
+	// Components
+
+	var likeComponent = Vue.extend({
+		template: '<button>like</button>'
+		//templateにかける要素は１つのみ
+		//複数かきたいときは
+		//<div><button>1</button><button>2</button></div>
+		//のように親要素で囲う
+	});
+	var app = new Vue({
 		el: '#app',
-		data:{
-			newItem: '',
-			todos:[]
-		},
-		watch: {
-			todos: {
-				handler: function(){
-					localStorage.setItem('todos', JSON.stringify(this.todos));
-				},
-				deep: true
-			}
-		},
-		mounted: function(){
-			this.todos = JSON.parse(localStorage.getItem('todos')) || [];
-		},
-		methods: {
-			// == TODOを追加する
-			// # submitしても画面遷移させない
-			// addItem: function(e){
-			// 	e.preventDefault();
-			// 	this.todos.push(this.newItem);
-			// }
-			// ->ディレクティブにpreventつけちゃう
-			addItem: function(){
-				var item = {
-					title: this.newItem,
-					isDone: false
-				};
-				this.todos.push(item); //pushするのはここのitem
-				this.newItem = ''; //そのままだと値が残っちゃうからここで空文字にする
-			},
-			// // == TODO削除
-			deleteItem: function(index){
-				if(confirm('OK?')){
-					this.todos.splice(index,1); //index番目から1個削除
-				}
-			},
-			// == 完了したTODOを一気に削除
-			purge: function() {
-				if(!confirm('delete finished?')){
-					return;
-				}
-				this.todos = this.remaining;
-			}
-		},
-		computed: {
-			remaining: function(){
-				return this.todos.filter(function(todo){
-					return !todo.isDone;
-				});
-			}
+		components : {
+			'like-component': likeComponent
 		}
 	});
+
 })();
